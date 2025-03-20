f5fb5741d66428464677e1c7170de49431f895d7
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-03-20 10:13:54 -0300

    - adds X and Google as login providers

--- apps/web/src/components/auth/SuperTokensProvider.tsx
+++ apps/web/src/components/auth/SuperTokensProvider.tsx
@@ -9,15 +9,15 @@ import { GameNodeLogo } from "@repo/ui";
 import { AuthRecipeComponentsOverrideContextProvider } from "supertokens-auth-react/ui";
 
 export const frontendConfig = (): SuperTokensConfig => {
-    return {
-        appInfo: {
-            appName: "GameNode",
-            apiDomain: process.env.NEXT_PUBLIC_DOMAIN_SERVER as string,
-            websiteDomain: process.env.NEXT_PUBLIC_DOMAIN_WEBSITE as string,
-            apiBasePath: "/v1/auth",
-            websiteBasePath: "/auth",
-        },
-        style: `
+  return {
+    appInfo: {
+      appName: "GameNode",
+      apiDomain: process.env.NEXT_PUBLIC_DOMAIN_SERVER as string,
+      websiteDomain: process.env.NEXT_PUBLIC_DOMAIN_WEBSITE as string,
+      apiBasePath: "/v1/auth",
+      websiteBasePath: "/auth",
+    },
+    style: `
         [data-supertokens~=container] {
             --palette-background: 28, 28, 28;
             --palette-inputBackground: 41, 41, 41;
@@ -33,67 +33,71 @@ export const frontendConfig = (): SuperTokensConfig => {
             --palette-textGray: 158, 158, 158;
         }
     `,
-        getRedirectionURL: async (context: any) => {
-            if (context.action === "SUCCESS" && context.newSessionCreated) {
-                if (context.redirectToPath !== undefined) {
-                    // we are navigating back to where the user was before they authenticated
-                    return context.redirectToPath;
-                }
-                if (context.createdNewUser) {
-                    // user signed up
-                    return "/wizard/init";
-                } else {
-                    // user signed in
-                }
-                return "/";
-            }
-            return undefined;
-        },
+    getRedirectionURL: async (context: any) => {
+      if (context.action === "SUCCESS" && context.newSessionCreated) {
+        if (context.redirectToPath !== undefined) {
+          // we are navigating back to where the user was before they authenticated
+          return context.redirectToPath;
+        }
+        if (context.createdNewUser) {
+          // user signed up
+          return "/wizard/init";
+        } else {
+          // user signed in
+        }
+        return "/";
+      }
+      return undefined;
+    },
 
-        recipeList: [
-            Passwordless.init({
-                contactMethod: "EMAIL",
-            }),
-            ThirdParty.init({
-                signInAndUpFeature: {
-                    providers: [ThirdParty.Discord.init()],
-                },
-            }),
-            Session.init(),
-        ],
-        windowHandler: (oI: any) => {
-            return {
-                ...oI,
-                location: {
-                    ...oI.location,
-                    setHref: (href: string) => {
-                        Router.push(href);
-                    },
-                },
-            };
+    recipeList: [
+      Passwordless.init({
+        contactMethod: "EMAIL",
+      }),
+      ThirdParty.init({
+        signInAndUpFeature: {
+          providers: [
+            ThirdParty.Discord.init(),
+            ThirdParty.Google.init(),
+            ThirdParty.Twitter.init(),
+          ],
         },
-    };
+      }),
+      Session.init(),
+    ],
+    windowHandler: (oI: any) => {
+      return {
+        ...oI,
+        location: {
+          ...oI.location,
+          setHref: (href: string) => {
+            Router.push(href);
+          },
+        },
+      };
+    },
+  };
 };
 
 if (typeof window !== "undefined") {
-    // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
-    SuperTokensReact.init(frontendConfig());
+  // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
+  SuperTokensReact.init(frontendConfig());
 }
 
 const SuperTokensProvider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <SuperTokensWrapper>
-            <AuthRecipeComponentsOverrideContextProvider
-                components={{
-                    AuthPageHeader_Override: (Default, ...props) => {
-                        return <GameNodeLogo style={{ marginBottom: 20 }} />;
-                    },
-                }}
-            >
-                {children}
-            </AuthRecipeComponentsOverrideContextProvider>
-        </SuperTokensWrapper>
-    );
+  return (
+    <SuperTokensWrapper>
+      <AuthRecipeComponentsOverrideContextProvider
+        components={{
+          AuthPageHeader_Override: (Default, ...props) => {
+            return <GameNodeLogo style={{ marginBottom: 20 }} />;
+          },
+        }}
+      >
+        {children}
+      </AuthRecipeComponentsOverrideContextProvider>
+    </SuperTokensWrapper>
+  );
 };
 
 export default SuperTokensProvider;
