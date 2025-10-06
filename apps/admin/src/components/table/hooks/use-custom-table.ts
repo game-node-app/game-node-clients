6569af73ffd45f74334c30a07c730b44a60c0202
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-10-05 23:18:07 -0300

    - versioning - eslint fixes

--- apps/admin/src/components/table/hooks/use-custom-table.ts
+++ apps/admin/src/components/table/hooks/use-custom-table.ts
@@ -1,60 +1,60 @@
 import {
-    type MRT_TableOptions,
-    useMantineReactTable,
+  type MRT_TableOptions,
+  useMantineReactTable,
 } from "mantine-react-table";
 
-export type CustomTableOptions<TData extends Record<string, any> = {}> = Omit<
-    MRT_TableOptions<TData>,
-    | "mantinePaginationProps"
-    | "paginationDisplayMode"
-    | "mantineTableProps.align"
-    | "mantinePaperProps"
-    | "initialState.density"
+export type CustomTableOptions<TData extends Record<string, unknown>> = Omit<
+  MRT_TableOptions<TData>,
+  | "mantinePaginationProps"
+  | "paginationDisplayMode"
+  | "mantineTableProps.align"
+  | "mantinePaperProps"
+  | "initialState.density"
 >;
 
-export const useCustomTable = <TData extends Record<string, any> = {}>(
-    tableOptions: CustomTableOptions<TData>,
+export const useCustomTable = <TData extends Record<string, unknown>>(
+  tableOptions: CustomTableOptions<TData>,
 ) => {
-    return useMantineReactTable({
-        ...{
-            paginationDisplayMode: "pages",
-            // styles
-            mantineTableProps: {
-                align: "center",
-            },
-            positionActionsColumn: "last",
-            mantinePaperProps: {
-                shadow: "0",
-                radius: "md",
-                p: "md",
-                withBorder: false,
-            },
-            displayColumnDefOptions: {
-                "mrt-row-actions": {
-                    size: 200, //make actions column wider
-                },
-            },
-            mantineFilterTextInputProps: {
-                style: { borderBottom: "unset", marginTop: "8px" },
-                variant: "filled",
-            },
-            mantineFilterSelectProps: {
-                style: { borderBottom: "unset", marginTop: "8px" },
-                variant: "filled",
-            },
-            // features
-            enableColumnActions: false,
-            enableDensityToggle: false,
-            enableFullScreenToggle: false,
-            enableHiding: false,
-            enablePinning: false,
-            // states
-            initialState: {
-                density: "md",
-            },
-            columns: [],
-            data: [],
+  return useMantineReactTable({
+    ...{
+      paginationDisplayMode: "pages",
+      // styles
+      mantineTableProps: {
+        align: "center",
+      },
+      positionActionsColumn: "last",
+      mantinePaperProps: {
+        shadow: "0",
+        radius: "md",
+        p: "md",
+        withBorder: false,
+      },
+      displayColumnDefOptions: {
+        "mrt-row-actions": {
+          size: 200, //make actions column wider
         },
-        ...tableOptions,
-    });
+      },
+      mantineFilterTextInputProps: {
+        style: { borderBottom: "unset", marginTop: "8px" },
+        variant: "filled",
+      },
+      mantineFilterSelectProps: {
+        style: { borderBottom: "unset", marginTop: "8px" },
+        variant: "filled",
+      },
+      // features
+      enableColumnActions: false,
+      enableDensityToggle: false,
+      enableFullScreenToggle: false,
+      enableHiding: false,
+      enablePinning: false,
+      // states
+      initialState: {
+        density: "md",
+      },
+      columns: [],
+      data: [],
+    },
+    ...tableOptions,
+  });
 };
