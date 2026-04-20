c0763dd06ce729a2c6c3503ecaabf4c9cbcc68bf
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2026-04-20 10:59:41 -0300

    feat: update configuration and dependencies in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,24 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingIncludes: {
+    "/*": [
+      "../../node_modules/@reduxjs/toolkit/**/*",
+      "../../node_modules/immer/**/*",
+      "../../node_modules/react-redux/**/*",
+      "../../node_modules/redux/**/*",
+      "../../node_modules/reselect/**/*",
+      "../../node_modules/use-sync-external-store/**/*",
+      "../../node_modules/recharts/**/*",
+    ],
+  },
   reactStrictMode: true,
   i18n: {
     locales: ["en", "pt-BR"],
